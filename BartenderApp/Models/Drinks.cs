1d847834485470c2a0746c57a1f0400857c7efdc
bennr9 <rachel.bennett16@yahoo.com>
2016-09-05 23:20:57 -0400

    Add project files.

---
+++ BartenderApp/Models/Drinks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BartenderApp.Models
+{
+    public class Drink
+    {
+        public int DrinkID { get; set; }
+        public string DrinkName { get; set; }
+        public int Quantity { get; set; }
+        public int Queue { get; set; }
+    }
+
+    public class DrinkDBContext : DbContext
+    {
+        public DbSet<Drink> Drinks { get; set; }
+    }
+}
